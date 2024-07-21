b88a3d3f8abd40f1c9f23327e971bc9c82d64513
Novpa <agungnovpa@gmail.com>
2024-07-21 20:37:21 +0800

    Adding Main menu

--- function.c
+++ function.c
@@ -1,5 +1,20 @@
-int increment(){
-static int count;
-  count++;
-  return count;
+//Welcome message function: 
+void printWelcomeMessage() {
+    printf("*********************************************************\n");
+    printf("*                                                       *\n");
+    printf("*                  W E L C O M E  T O                   *\n");
+    printf("*                                                       *\n");
+    printf("*                  T o - C - S h o p                    *\n");
+    printf("*                                                       *\n");
+    printf("*        Manage your shopping list efficiently!         *\n");
+    printf("*                                                       *\n");
+    printf("*    +---------------------------------------------+    *\n");
+    printf("*    | 1. Add list                                 |    *\n");
+    printf("*    | 2. Show list                                |    *\n");
+    printf("*    | 3. Remove list / Mark                       |    *\n");
+    printf("*    | 4. Search list                              |    *\n");
+    printf("*    | 5. Exit                                     |    *\n");
+    printf("*    +---------------------------------------------+    *\n");
+    printf("*                                                       *\n");
+    printf("*********************************************************\n");
 }
--- main.c
+++ main.c
@@ -1,123 +1,134 @@
 #include <stdio.h>
+
 #include <string.h>
+
 #include <stdlib.h>
 
 //Clear the buffer function:
 void clearInputBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF);
 }
 
 //Remove the new line if exist function:
-void removeNewline(char *str) {
-    size_t len = strlen(str);
-    if (len > 0 && str[len-1] == '\n') {
-        str[len-1] = '\0';
-    }
+void removeNewline(char * str) {
+  size_t len = strlen(str);
+  if (len > 0 && str[len - 1] == '\n') {
+    str[len - 1] = '\0';
+  }
 }
 
 //Clear console function: 
 void clearConsole() {
-    system("cls");
+  system("cls");
 }
 
 //Welcome message function: 
-void printWelcomeMessage() {
-    printf("*********************************************************\n");
-    printf("*                                                       *\n");
-    printf("*                  W E L C O M E  T O                   *\n");
-    printf("*                                                       *\n");
-    printf("*                  T o - C - S h o p                    *\n");
-    printf("*                                                       *\n");
-    printf("*        Manage your shopping list efficiently!         *\n");
-    printf("*                                                       *\n");
-    printf("*    +---------------------------------------------+    *\n");
-    printf("*    | 1. Add list                                 |    *\n");
-    printf("*    | 2. Show list                                |    *\n");
-    printf("*    | 3. Remove list / Mark                       |    *\n");
-    printf("*    | 4. Search list                              |    *\n");
-    printf("*    | 5. Exit                                     |    *\n");
-    printf("*    +---------------------------------------------+    *\n");
-    printf("*                                                       *\n");
-    printf("*********************************************************\n");
-}
+void printWelcomeMessage();
 
-typedef struct shoppingInput
-{
+typedef struct shoppingInput {
   int numberOfItem;
   char nameOfItem[265];
   int priceOfItem;
   int totalPriceOfItem;
-}Shopping;
+}
+Shopping;
 
-void inputFunction (Shopping *data){
+void inputFunction(Shopping * data) {
   clearConsole();
   printf("\n");
+  printf("*** Press 0 to Exit ***\n\n");
   printf("Total item: ");
-  scanf("%d", &data->numberOfItem);
+  scanf("%d", & data -> numberOfItem);
 
-  if(data->numberOfItem == 0){
+  if (data -> numberOfItem == 0) {
     return;
   }
 
   clearInputBuffer();
   printf("Name of item: ");
-  fgets(data->nameOfItem, sizeof(data->nameOfItem), stdin);
-  removeNewline(data->nameOfItem);
+  fgets(data -> nameOfItem, sizeof(data -> nameOfItem), stdin);
+  removeNewline(data -> nameOfItem);
   printf("Input approx. Price of item: ");
-  scanf("%d", &data->priceOfItem);
+  scanf("%d", & data -> priceOfItem);
   printf("\n");
 }
 
-void printOutput (Shopping *data, int currentSize){
+void printOutput(Shopping * data, int currentSize) {
   clearConsole();
-  for(int i = 0; i < currentSize; i++){
+  printf("Your Shopping List :\n\n");
+  for (int i = 0; i < currentSize; i++) {
     printf("-> %d %s %d\n", data[i].numberOfItem, data[i].nameOfItem, data[i].priceOfItem);
   }
+  printf("\n");
 }
 
 //Malloc function:
-void memorryAllocation (Shopping **shoppingData, int currentSize){
-  *shoppingData = (Shopping*)malloc(currentSize * sizeof(Shopping));
-  if(*shoppingData == NULL){
+void memorryAllocation(Shopping ** shoppingData, int currentSize) {
+  * shoppingData = (Shopping * ) malloc(currentSize * sizeof(Shopping));
+  if ( * shoppingData == NULL) {
     printf("Memory allocation failed\n");
-        exit(1);
+    exit(1);
   }
 }
 
 //Realloc function:
-void memorryReallocation (Shopping **shoppingData, int size){
-  *shoppingData = (Shopping*)realloc(*shoppingData, size * sizeof(Shopping));
-  if(*shoppingData == NULL){
+void memorryReallocation(Shopping ** shoppingData, int size) {
+  * shoppingData = (Shopping * ) realloc( * shoppingData, size * sizeof(Shopping));
+  if ( * shoppingData == NULL) {
     printf("Memory reallocation failed\n");
-        exit(1);
+    exit(1);
   }
 }
 
-int  main(){
+int main() {
 
-Shopping *shoppingData = NULL;
-int size = 1;
-int currentSize = 0;
+  Shopping * shoppingData = NULL;
+  int size = 1;
+  int currentSize = 0;
+  int option, next;
+  //Option function
+  do{
+    int optionMainMenu(int option) {
+      printWelcomeMessage();
+      printf("Chose the menu: ");
+      scanf("%d", & option);
+      switch (option) {
+      case 1:
+        memorryAllocation( & shoppingData, size);
 
-memorryAllocation(&shoppingData, size);
+        while (1) {
+          if (currentSize == size) {
+            size *= 2;
+            memorryReallocation( & shoppingData, size);
+          }
 
-while(1){
-if(currentSize == size){
-  size *= 2;
-  memorryReallocation(&shoppingData, size);
-}
+          inputFunction( & shoppingData[currentSize]);
 
-inputFunction(&shoppingData[currentSize]);
+          if (shoppingData[currentSize].numberOfItem == 0) {
+            clearConsole();
+            optionMainMenu(option);
+          }
+          currentSize++;
+        }
 
-if(shoppingData[currentSize].numberOfItem == 0) break;
-currentSize++;
-}
+      case 2:
+      printOutput(shoppingData, currentSize);
 
-printOutput(shoppingData, currentSize);
+      default:
+      printf("Choose the correct menu! Press 1 to continue...");
+      scanf("%d", &next);
+      clearConsole();
+      optionMainMenu(option);
+      }
 
-free(shoppingData);
-shoppingData = NULL;
+    }
+    optionMainMenu(option);
+  }while(option != 0);
+  
 
-return 0;
+  free(shoppingData);
+  shoppingData = NULL;
+
+  return 0;
 }
